c731c0036c377d101949ad9c5d639d2186a0d928
woodsfsg <130228546+woodsfsg@users.noreply.github.com>
2025-11-07 09:40:10 +0000

    Modify donor sensor.py from MPU6886 component to suit ICM20948

--- components/icm20948/sensor.py
+++ components/icm20948/sensor.py
@@ -5,15 +5,17 @@ from esphome.const import (
     CONF_ID,
     CONF_TEMPERATURE,
     DEVICE_CLASS_TEMPERATURE,
-    ICON_BRIEFCASE_DOWNLOAD,
+    ICON_ICON_ACCELERATION,
     STATE_CLASS_MEASUREMENT,
     UNIT_METER_PER_SECOND_SQUARED,
     ICON_SCREEN_ROTATION,
     UNIT_DEGREE_PER_SECOND,
     UNIT_CELSIUS,
+    UNIT_DEGREES,
+    ICON_ROTATE_RIGHT,
 )
 
-CODEOWNERS = ["@fabaff"]
+CODEOWNERS = ["@woodsfsg"]
 DEPENDENCIES = ["i2c"]
 
 CONF_ACCEL_X = "accel_x"
@@ -22,21 +24,30 @@ CONF_ACCEL_Z = "accel_z"
 CONF_GYRO_X = "gyro_x"
 CONF_GYRO_Y = "gyro_y"
 CONF_GYRO_Z = "gyro_z"
+CONF_MAG_X = "mag_x"
+CONF_MAG_Y = "mag_y"
+CONF_MAG_Z = "mag_z"
 
-mpu6886_ns = cg.esphome_ns.namespace("mpu6886")
-MPU6886Component = mpu6886_ns.class_(
-    "MPU6886Component", cg.PollingComponent, i2c.I2CDevice
+icm20948_ns = cg.esphome_ns.namespace("icm20948")
+ICM20948Component = icm20948_ns.class_(
+    "ICM20948Component", cg.PollingComponent, i2c.I2CDevice
 )
 
 accel_schema = sensor.sensor_schema(
     unit_of_measurement=UNIT_METER_PER_SECOND_SQUARED,
-    icon=ICON_BRIEFCASE_DOWNLOAD,
+    icon=ICON_ACCELERATION,
     accuracy_decimals=2,
     state_class=STATE_CLASS_MEASUREMENT,
 )
 gyro_schema = sensor.sensor_schema(
     unit_of_measurement=UNIT_DEGREE_PER_SECOND,
     icon=ICON_SCREEN_ROTATION,
+    accuracy_decimals=2,
+    state_class=STATE_CLASS_MEASUREMENT,
+)
+mag_schema = sensor.sensor_schema(
+    unit_of_measurement=UNIT_DEGREES,
+    icon=ICON_ROTATE_RIGHT,
     accuracy_decimals=2,
     state_class=STATE_CLASS_MEASUREMENT,
 )
@@ -50,13 +61,16 @@ temperature_schema = sensor.sensor_schema(
 CONFIG_SCHEMA = (
     cv.Schema(
         {
-            cv.GenerateID(): cv.declare_id(MPU6886Component),
+            cv.GenerateID(): cv.declare_id(ICM20948Component),
             cv.Optional(CONF_ACCEL_X): accel_schema,
             cv.Optional(CONF_ACCEL_Y): accel_schema,
             cv.Optional(CONF_ACCEL_Z): accel_schema,
             cv.Optional(CONF_GYRO_X): gyro_schema,
             cv.Optional(CONF_GYRO_Y): gyro_schema,
             cv.Optional(CONF_GYRO_Z): gyro_schema,
+            cv.Optional(CONF_MAG_X): mag_schema,
+            cv.Optional(CONF_MAG_Y): mag_schema,
+            cv.Optional(CONF_MAG_Z): mag_schema,
             cv.Optional(CONF_TEMPERATURE): temperature_schema,
         }
     )
@@ -71,14 +85,18 @@ async def to_code(config):
     await i2c.register_i2c_device(var, config)
 
     for d in ["x", "y", "z"]:
-        accel_key = f"accel_{d}"
-        if accel_key in config:
-            sens = await sensor.new_sensor(config[accel_key])
+        dim_key = f"accel_{d}"
+        if dim_key in config:
+            sens = await sensor.new_sensor(config[dim_key])
             cg.add(getattr(var, f"set_accel_{d}_sensor")(sens))
-        accel_key = f"gyro_{d}"
-        if accel_key in config:
-            sens = await sensor.new_sensor(config[accel_key])
+        dim_key = f"gyro_{d}"
+        if dim_key in config:
+            sens = await sensor.new_sensor(config[dim_key])
             cg.add(getattr(var, f"set_gyro_{d}_sensor")(sens))
+        dim_key = f"mag_{d}"
+        if dim_key in config:
+            sens = await sensor.new_sensor(config[dim_key])
+            cg.add(getattr(var, f"set_mag_{d}_sensor")(sens))
 
     if CONF_TEMPERATURE in config:
         sens = await sensor.new_sensor(config[CONF_TEMPERATURE])
